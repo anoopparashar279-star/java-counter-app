bf9e38ce0b48f2b97629ba5fd0f170d4fcda1d77
anoop <anoopparashar279@gmail.com>
2026-09-13 14:33:37 +0530

    first commit

---
+++ src/CounterApp.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class CounterApp {
+
+    static int count = 0;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\nCurrent count: " + count);
+            System.out.println("1. Increment");
+            System.out.println("2. Decrement");
+            System.out.println("3. Reset");
+            System.out.println("4. Exit");
+
+            System.out.print("Choose option: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    count++;
+                    break;
+
+                case 2:
+                    count--;
+                    break;
+
+                case 3:
+                    count = 0;
+                    break;
+
+                case 4:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
+}
